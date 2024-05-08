9926c2ee899de15e484224fdc313ea166bb43697
shdab <130516923+shadatr@users.noreply.github.com>
2024-05-08 22:36:59 +0300

    create And Initialize Tree function added

--- src/index.ts
+++ src/index.ts
@@ -37,9 +37,85 @@ dotenv.config()
 async function main() {
   const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
   const wallet = await getOrCreateKeypair("Wallet_1")
-  airdropSolIfNeeded(wallet.publicKey)
+  await airdropSolIfNeeded(wallet.publicKey)
+
+  const maxDepthSizePair: ValidDepthSizePair = {
+    maxDepth: 3,
+    maxBufferSize: 8,
+  }
+
+  const canopyDepth = 0
+
+  const treeAddress = await createAndInitializeTree(
+    connection,
+    wallet,
+    maxDepthSizePair,
+    canopyDepth
+  )
 }
 
 // Demo Code Here
+
+async function createAndInitializeTree(
+  connection: Connection,
+  payer: Keypair,
+  maxDepthSizePair: ValidDepthSizePair,
+  canopyDepth: number
+) {
+	const treeKeypair = Keypair.generate()
+
+	const allocTreeIx = await createAllocTreeIx(
+    connection,
+    treeKeypair.publicKey,
+    payer.publicKey,
+    maxDepthSizePair,
+    canopyDepth
+  )
+
+	const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
+    [treeKeypair.publicKey.toBuffer()],
+    BUBBLEGUM_PROGRAM_ID
+  )
+
+	const createTreeIx = createCreateTreeInstruction(
+    {
+      treeAuthority,
+      merkleTree: treeKeypair.publicKey,
+      payer: payer.publicKey,
+      treeCreator: payer.publicKey,
+      logWrapper: SPL_NOOP_PROGRAM_ID,
+      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+    },
+    {
+      maxBufferSize: maxDepthSizePair.maxBufferSize,
+      maxDepth: maxDepthSizePair.maxDepth,
+      public: false,
+    }
+  )
+
+	const tx = new Transaction().add(allocTreeIx, createTreeIx)
+  tx.feePayer = payer.publicKey
+  
+  try {
+    const txSignature = await sendAndConfirmTransaction(
+      connection,
+      tx,
+      [treeKeypair, payer],
+      {
+        commitment: "confirmed",
+        skipPreflight: true,
+      }
+    )
+
+    console.log(`https://explorer.solana.com/tx/${txSignature}?cluster=devnet`)
+
+    console.log("Tree Address:", treeKeypair.publicKey.toBase58())
+
+    return treeKeypair.publicKey
+  } catch (err: any) {
+    console.error("\nFailed to create merkle tree:", err)
+    throw err
+  }
+}
 
 main()
